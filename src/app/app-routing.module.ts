7d7303a9ca8cb5e47758e1aba339d79d2ae67e87
Gary Alfred <gary.alfred@gmail.com>
2022-03-11 15:14:11 -0500

    added submodule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'group-a',
+    loadChildren: () => import('./group-a/group-a.module').then((mod) => mod.GroupAModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
